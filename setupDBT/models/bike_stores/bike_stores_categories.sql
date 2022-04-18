881d33fecf8baca70cc0025be1e27a9a4300baec
Ketan Sahu <k.sahu@brainbay.nl>
2022-04-18 17:08:27 +0200

    DBT project setup

---
+++ setupDBT/models/bike_stores/bike_stores_categories.sql
@@ -0,0 +1,12 @@
+
+{{ config(materialized='table') }}
+
+WITH source_categories AS (
+    SELECT * FROM {{ source('production_store', 'categories') }}
+    ),
+
+    FINAL AS (
+        SELECT * FROM source_categories
+    )
+
+    SELECT * FROM FINAL
